bae6e49df38e502a55f4319f4ca2022c20c9c24f
Chronoss0518 <sinyusuke5000@gmail.com>
2025-01-03 18:35:12 +0900

    Prefabフォルダを作成し、CardPrefabを移動

--- Assets/Prefab/Card/CardScript.cs
+++ Assets/Prefab/Card/CardScript.cs
@@ -14,13 +14,12 @@ public class CardScript : MonoBehaviour
     [SerializeField]
     RawImage front = null, back = null;
 
-    public string cardName { get; private set; } = "";
+    public string name { get; private set; } = "";
+
+    public string description { get; private set; } = "";
 
     public CardType type { get; private set; } = CardType.Magic;
 
-
     public void SetFrontTexture(Texture _tex){ if (front != null) front.texture = _tex; }
     public void SetBackTexture(Texture _tex) { if (back != null) back.texture = _tex; }
-    
-
 }
