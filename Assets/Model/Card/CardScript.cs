1822880e074a9f408924bb271b84608ebf555d3f
Chronoss0518 <sinyusuke5000@gmail.com>
2025-01-03 16:31:02 +0900

    ModelにCardの土台となるPrefabを追加

---
+++ Assets/Model/Card/CardScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardScript : MonoBehaviour
+{
+    public enum CardType
+    {
+        Magic,
+        Item,
+    }
+
+    [SerializeField]
+    RawImage front = null, back = null;
+
+    public string cardName { get; private set; } = "";
+
+    public CardType type { get; private set; } = CardType.Magic;
+
+
+    public void SetFrontTexture(Texture _tex){ if (front != null) front.texture = _tex; }
+    public void SetBackTexture(Texture _tex) { if (back != null) back.texture = _tex; }
+    
+
+}
