de5717c396be553000a1e789772b4363b6e93ed2
Chronoss0518 <sinyusuke5000@gmail.com>
2025-01-11 15:31:41 +0900

    Page側のページ情報を取得できるように修正

--- Assets/Script/Books/Page.cs
+++ Assets/Script/Books/Page.cs
@@ -11,4 +11,26 @@ public class Page : MonoBehaviour
 
     [SerializeField]
     private GameObject[] backCardSocket = null;
+
+    public GameObject[] getFrontCardSocket { get { return frontCardSocket; } }
+    
+    public GameObject[] getBackCardSocket { get { return backCardSocket; } }
+
+    public void SetFrontPageActive(bool _active)
+    {
+        SetPageActive(_active, frontCardSocket);
+    }
+
+    public void SetBackPageActive(bool _active)
+    {
+        SetPageActive(_active, backCardSocket);
+    }
+
+    void SetPageActive(bool _active, GameObject[] _socket)
+    {
+        foreach (var obj in _socket)
+        {
+            obj.SetActive(_active);
+        }
+    }
 }
