a69971ab4263f53c7ea102b920866fa865ca00fb
Chronoss0518 <sinyusuke5000@gmail.com>
2025-01-15 23:31:05 +0900

    CardTypeをCardDataクラスへ移動

--- Assets/Script/Game/CardScripts/CardScript.cs
+++ Assets/Script/Game/CardScripts/CardScript.cs
@@ -1,31 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CardScript : MonoBehaviour
 {
-    public enum CardType
-    {
-        Magic,
-        Item,
-    }
-
     [SerializeField]
     RawImage front = null, back = null;
 
-    public string name { get; private set; } = "";
+    [SerializeField,ReadOnly]
+    CardData data = null;
 
-    public string description { get; private set; } = "";
+    public string cardName { get { return data.name; } }
 
-    public int initBookPos { get; private set; } = 0;
+    public string description { get { return data.description; } }
 
-    public CardType type { get; private set; } = CardType.Magic;
+    public int[] script { get { return data.script; } }
+
+    public int initBookPos { get { return data.initBookPos; } }
+
+    public CardData.CardType type { get; private set; } = CardData.CardType.Magic;
 
     public void SetFrontTexture(Texture2D _tex){ if (front != null) front.texture = _tex; }
     public void SetBackTexture(Texture2D _tex) { if (back != null) back.texture = _tex; }
 
-    public void SetInitBookPos(int _pos) { initBookPos = _pos; }
+    public void Init(CardData _data)
+    {
+        data = _data;
+        InitMagicCardScript(data);
+        InitItemCardScript(data);
+    }
 
-    
+    void InitMagicCardScript(CardData _data)
+    {
+        if (data.cardType != (int)CardData.CardType.Magic) return;
+        var script = gameObject.AddComponent<MagicCardScript>();
+        var magicData = (MagicCardData)_data;
+        script.SetAttribute(magicData.month);
+        script.SetPoint(magicData.point);
+        script.SetBaseCard(this);
+    }
+
+    void InitItemCardScript(CardData _data)
+    {
+        if (data.cardType != (int)CardData.CardType.Item) return;
+        var script = gameObject.AddComponent<ItemCardScript>();
+        var itemData = (ItemCardData)_data;
+        script.SetType((ItemCardScript.ItemType)itemData.itemType);
+        script.SetBaseCard(this);
+    }
+
+
 }
