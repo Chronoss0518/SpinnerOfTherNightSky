d10ff5b0ec41c199160cb2ab76118e22706db9d8
Chronoss0518 <sinyusuke5000@gmail.com>
2025-01-18 17:24:41 +0900

    一旦ControllerBaseをコミット

---
+++ Assets/Script/Game/Controller/ControllerBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class ControllerBase : MonoBehaviour
+{
+    public bool isAction { get; private set; } = false;
+
+    
+
+}
