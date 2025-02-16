ac2fe992e3eb34629af9d4ced5e12b927104dfc1
Chronoss0518 <sinyusuke5000@gmail.com>
2025-02-16 13:52:01 +0900

    ・ScriptTypeからFlagsを削除 ・ActionTypeを追加

--- Assets/Script/Game/ScriptManager.cs
+++ Assets/Script/Game/ScriptManager.cs
@@ -5,37 +5,32 @@ using UnityEngine;
 [System.Serializable]
 public class ScriptManager
 {
-    [System.Flags]
     public enum ScriptType : int
     {
-        PutStone = 1,//石を置く//
-        RemoveStone = 2,//石を取り除く//
-        SelectPlayer = 4,//Playerを選択する//
-        SelectCardBook = 8,//魔導書からカードを選択する//
-        SelectCardItem = 16,//選択したPlayerのItemZoneのカードを選択する//
-        SelectCardItemZone = 32,//選択したPlayerのItemZoneの場所を選択する//
-        SelectCardTrash = 64,//選択したPlayerのTrashZoneのカードを選択する//
-        SelectCardMagic = 128,//選択したPlayerのMagicZoneのカードを選択する//
-        MoveSetItem = 256,//ItemZoneへカードを伏せて出す//
-        MoveOpenItem = 512,//ItemZoneへカードを公開して出す//
-        MoveMagic = 1024,//MagicZoneへカードを出す//
-        MoveBook = 2048,//魔導書へカードを戻す//
-        MoveTrash = 4096,//TrashZoneへカードを出す//
-        Stack = 8192,//カードをStackする→カードの発動準備//
+        PutStone,//石を置く//
+        RemoveStone,//石を取り除く//
+        SelectPlayer,//Playerを選択する//
+        SelectCardBook,//魔導書からカードを選択する//
+        SelectCardItem,//選択したPlayerのItemZoneのカードを選択する//
+        SelectCardItemZone,//選択したPlayerのItemZoneの場所を選択する//
+        SelectCardTrash,//選択したPlayerのTrashZoneのカードを選択する//
+        SelectCardMagic,//選択したPlayerのMagicZoneのカードを選択する//
+        MoveSetItem,//ItemZoneへカードを伏せて出す//
+        MoveOpenItem,//ItemZoneへカードを公開して出す//
+        MoveMagic,//MagicZoneへカードを出す//
+        MoveBook,//魔導書へカードを戻す//
+        MoveTrash,//TrashZoneへカードを出す//
+        Stack,//カードをStackする→カードの発動準備//
+        UpWinnerPoint,//ゲームに勝利するためのポイントが上がる
     }
 
-    [System.Serializable]
-    public class ScriptData
+    public enum ActionType : int
     {
-        public ScriptType type = ScriptType.PutStone;
-        public byte argment = 0;
+        Entry,
+        Stay,
+        Exit,
     }
 
-    [System.Serializable]
-    public class ScriptList
-    {
-        public List<ScriptData>scriptList = new List<ScriptData>();
-    }
 
     public bool isRunScript { get; private set; } = false;
 
@@ -47,6 +42,7 @@ public class ScriptManager
 
     CardScript targetCard { get; set; } = null;
 
+    Dictionary<ScriptType,CardScript> stayActionCardScripts = new Dictionary<ScriptType, CardScript>();
 
 #if UNITY_EDITOR
 
@@ -64,26 +60,27 @@ public class ScriptManager
 
 #endif
 
-    public void RunScript(Player _player, ScriptList _scriptList)
+    public void IsStayScriptTest()
     {
 
     }
 
-    public void RunScript(Player _player, ScriptData _script)
+
+    public void RunScript(Player _player, ScriptParts _script)
     {
         PutStone(_player, _script);
         RemoveStone(_player, _script);
     }
 
 
-    void PutStone(Player _player, ScriptData _script)
+    void PutStone(Player _player, ScriptParts _script)
     {
         if (_script.type != ScriptType.PutStone) return;
 
 
     }
 
-    void RemoveStone(Player _player, ScriptData _script)
+    void RemoveStone(Player _player, ScriptParts _script)
     {
         if (_script.type != ScriptType.RemoveStone) return;
 
@@ -91,42 +88,42 @@ public class ScriptManager
     }
 
 
-    void SelectCardFromMyBook(Player _player, ScriptData _script)
+    void SelectCardFromMyBook(Player _player, ScriptParts _script)
     {
 
     }
 
-    void SelectCardFromMyMagic(Player _player, ScriptData _script)
+    void SelectCardFromMyMagic(Player _player, ScriptParts _script)
     {
 
     }
 
-    void SelectCardFromMyTrash(Player _player, ScriptData _script)
+    void SelectCardFromMyTrash(Player _player, ScriptParts _script)
     {
 
     }
 
-    void SelectCardFromMyItem(Player _player, ScriptData _script)
+    void SelectCardFromMyItem(Player _player, ScriptParts _script)
     {
 
     }
 
-    void SelectCardFromOtherMagic(Player _player, ScriptData _script)
+    void SelectCardFromOtherMagic(Player _player, ScriptParts _script)
     {
 
     }
 
-    void SelectCardFromOtherTrash(Player _player, ScriptData _script)
+    void SelectCardFromOtherTrash(Player _player, ScriptParts    _script)
     {
 
     }
 
-    void SelectCardFromOtherItem(Player _player, ScriptData _script)
+    void SelectCardFromOtherItem(Player _player, ScriptParts _script)
     {
 
     }
 
-    void SelectStone(Player _player, ScriptData _script)
+    void SelectStone(Player _player, ScriptParts _script)
     {
 
     }
