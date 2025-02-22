418f840852494dd11e81756835e4c482836323ad
Chronoss0518 <sinyusuke5000@gmail.com>
2025-02-22 17:24:47 +0900

    ControllerBaseにアクション開始時とアクション終了時の機能を追加

--- Assets/Script/Game/Controller/ControllerBase.cs
+++ Assets/Script/Game/Controller/ControllerBase.cs
@@ -6,6 +6,21 @@ abstract public class ControllerBase : MonoBehaviour
 {
     public bool isAction { get; private set; } = false;
 
-    
+    public void Action() { isAction = true; }
+
+    public void ActionStart() 
+    {
+        isAction = false;
+        RunActionStart();
+    }
+
+    public void ActionEnd()
+    {
+        isAction = false;
+        RunActionEnd();
+    }
+
+    virtual protected void RunActionStart() { }
+    virtual protected void RunActionEnd() { }
 
 }
