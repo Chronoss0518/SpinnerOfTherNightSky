b14a3176276e9d4d2a436f25dbcb3bed7d759ce7
Chronoss0518 <sinyusuke5000@gmail.com>
2025-03-17 20:32:01 +0900

    UI部分のクラスをスクリプトへ修正

---
+++ Assets/Script/Game/PlayerControllerUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControllerUI : MonoBehaviour
+{
+    [SerializeField]
+    public Text descriptionText = null;
+
+    [SerializeField]
+    public Text selectButtonText = null;
+
+    [SerializeField]
+    public Canvas buttonVisibleCanvas = null;
+
+    [SerializeField]
+    public Canvas buttonVisibleCanvasController = null;
+}
