b216cbe257a7f1bf063f443c3f6800c98ecc10fd
Chronoss0518 <sinyusuke5000@gmail.com>
2025-03-30 16:45:58 +0900

    SetTrapクラスの作成

---
+++ Assets/Script/Game/Manager/TurnClasss/SetTrap.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using Unity.Collections;
+
+[System.Serializable]
+public class SetTrap : TurnManager.TurnClass
+{
+    public SetTrap(TurnManager _manager) : base(_manager)
+    {
+        selectSetItem = gameManager.CreateScript(new ScriptData(
+            new ScriptParts[] {
+                new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--player-type 1 --min 0 --max 1 --zone-type-book --card-type 6 --normal-playing"),
+               new ScriptParts((int)ScriptManager.ScriptType.Stack, ""),},
+            ScriptManager.ActionType.Entry));
+    }
+
+    [SerializeField, ReadOnly]
+    ScriptManager.ScriptActionData selectSetItem = null;
+
+    int beforeSetCount = 0;
+
+    public override void Init()
+    {
+        base.Init();
+        var player = gameManager.GetNowPlayer();
+        beforeSetCount = player.itemZone.nowPutCount;
+    }
+
+    public override void Update()
+    {
+
+        var player = gameManager.GetNowPlayer();
+        int tmp = player.itemZone.nowPutCount;
+
+        if (tmp >= ItemZoneManager.PUT_ITEM_COUNT)
+        {
+            ChangeTurn();
+            return;
+        }
+
+        if (beforeSetCount == tmp)
+        {
+            ChangeTurn();
+            return;
+        }
+
+        gameManager.RegistScript(selectSetItem);
+
+    }
+
+    public override void Next()
+    {
+        SetMainStep(TurnManager.MainStep.EndTurn);
+    }
+}
