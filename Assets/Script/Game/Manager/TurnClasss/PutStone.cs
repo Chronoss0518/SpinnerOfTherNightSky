a2f8234462ce69a6dcad8e7d4b3ffbf43936c55d
Chronoss0518 <sinyusuke5000@gmail.com>
2025-03-30 16:18:34 +0900

    PutStoneクラスを仮作成

---
+++ Assets/Script/Game/Manager/TurnClasss/PutStone.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Unity.Collections;
+
+[System.Serializable]
+public class PutStone : TurnManager.TurnClass
+{
+    public PutStone(TurnManager _manager) : base(_manager)
+    {
+        selectStone = gameManager.CreateScript(new ScriptData(
+            new ScriptParts[] {
+                new ScriptParts((int)ScriptManager.ScriptType.SelectStoneBoard, "--min 1 --max 3 --is-put"),
+               new ScriptParts((int)ScriptManager.ScriptType.MoveStone, "--put"),},
+            ScriptManager.ActionType.Entry));
+    }
+
+    [SerializeField, ReadOnly]
+    ScriptManager.ScriptActionData selectStone = null;
+
+    public override void Update()
+    {
+        gameManager.RegistScript(selectStone);
+    }
+
+    public override void Next()
+    {
+        SetMainStep(TurnManager.MainStep.PlayMagic);
+    }
+}
