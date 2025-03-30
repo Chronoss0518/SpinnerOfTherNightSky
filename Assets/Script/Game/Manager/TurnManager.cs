383d9de8e44eea48f600c5a192a9f625c5103656
Chronoss0518 <sinyusuke5000@gmail.com>
2025-03-30 16:55:27 +0900

    Turn処理を各Turnクラスに譲渡

--- Assets/Script/Game/Manager/TurnManager.cs
+++ Assets/Script/Game/Manager/TurnManager.cs
@@ -37,18 +37,9 @@ public class TurnManager
         StartTurn,
         UseItem,
         PutStone,
-        PlayMagicInit,
         PlayMagic,
         SetTrap,
         EndTurn
-    }
-
-    public enum PlayMagicStep
-    {
-        StartStep,
-        SelectCard,
-        UseCard,
-        EndStep
     }
 
     GameManager gameManager = null;
@@ -59,227 +50,39 @@ public class TurnManager
 
     TurnClass runTurn = null;
 
-    [SerializeField, ReadOnly]
-    ScriptManager.ScriptActionData selectItem = null;
-
     [SerializeField,ReadOnly]
-    ScriptManager.ScriptActionData selectTrap = null;
+    private MainStep mainStep = MainStep.StartTurn;
 
-    [SerializeField, ReadOnly]
-    ScriptManager.ScriptActionData selectStone = null;
-
-    [SerializeField, ReadOnly]
-    ScriptManager.ScriptActionData selectMagic = null;
-
-    [SerializeField, ReadOnly]
-    ScriptManager.ScriptActionData selectCard = null;
-
-    [SerializeField, ReadOnly]
-    ScriptManager.ScriptActionData selectSetItem = null;
-
-    public MainStep mainStep { get; private set; } = MainStep.StartTurn;
-    public PlayMagicStep playMagicStep { get; private set; } = PlayMagicStep.EndStep;
-
-    [SerializeField, ReadOnly]
-    int tmpNowPlayerCount = 0;
-
-    [SerializeField, ReadOnly]
-    int playCardUserCount = 0;
-
-    [SerializeField, ReadOnly]
-    int otherPlayerCount = 0;
-
-    [SerializeField, ReadOnly]
-    int testPlayMagicCount = 0;
-
-    [SerializeField, ReadOnly]
-    int testPlayCardCount = 0;
+    public MainStep nowMainStep { get { return mainStep; } }
 
     public void Init(GameManager _gm)
     {
         gameManager = _gm;
         mainStep = MainStep.StartTurn;
-        playMagicStep = PlayMagicStep.EndStep;
 
-        turnClass =  new TurnClass[] { new StartTurn(this), };
+        turnClass =  new TurnClass[] { 
+            new StartTurn(this),
+            new UseItem(this),
+            new PutStone(this),
+            new PlayMagic(this),
+            new SetTrap(this),
+            new EndTurn(this)
+        };
 
         runTurn = turnClass[(int)mainStep];
-
-        selectItem = gameManager.CreateScript(new ScriptData(
-            new ScriptParts[] {
-                new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--min 0 --max 1 --player-type 0 --zone-type-book --card-type 2 --normal-playing"),
-               new ScriptParts((int)ScriptManager.ScriptType.MoveCard, "--open-item-zone"),
-                new ScriptParts((int)ScriptManager.ScriptType.Stack, ""),},
-            ScriptManager.ActionType.Entry));
-
-        selectTrap = gameManager.CreateScript(new ScriptData(
-            new ScriptParts[] {
-                new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--min 0 --max 1 --player-type 0 --zone-type-item --card-type 4 --normal-playing"),
-               new ScriptParts((int)ScriptManager.ScriptType.MoveCard, "--open-item-zone"),
-                new ScriptParts((int)ScriptManager.ScriptType.Stack, ""),},
-            ScriptManager.ActionType.Entry));
-
-        selectStone = gameManager.CreateScript(new ScriptData(
-            new ScriptParts[] {
-                new ScriptParts((int)ScriptManager.ScriptType.SelectStoneBoard, "--min 1 --max 3 --is-put"),
-               new ScriptParts((int)ScriptManager.ScriptType.MoveStone, "--put"),},
-            ScriptManager.ActionType.Entry));
-
-        selectMagic = gameManager.CreateScript(new ScriptData(
-            new ScriptParts[] {
-                new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--player-type 1 --min 0 --max 1 --zone-type-book --card-type 1 --normal-playing"),
-               new ScriptParts((int)ScriptManager.ScriptType.Stack, ""),},
-            ScriptManager.ActionType.Entry));
-
-        selectCard = gameManager.CreateScript(new ScriptData(
-            new ScriptParts[] {
-                new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--player-type 1 --min 0 --max 1 --zone-type-book --zone-type-item --card-type 5 --normal-playing"),
-               new ScriptParts((int)ScriptManager.ScriptType.MoveCard, "--open-item-zone"),
-                new ScriptParts((int)ScriptManager.ScriptType.Stack, ""),},
-            ScriptManager.ActionType.Entry));
-
-        selectSetItem = gameManager.CreateScript(new ScriptData(
-            new ScriptParts[] {
-                new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--player-type 1 --min 0 --max 1 --zone-type-book --card-type 6 --normal-playing"),
-               new ScriptParts((int)ScriptManager.ScriptType.Stack, ""),},
-            ScriptManager.ActionType.Entry));
     }
 
     public void Update()
     {
-        if(changeTurn)
-        {
-            runTurn.Next();
-            runTurn = null;
-            runTurn = turnClass[(int)mainStep];
-            runTurn.Init();
-            changeTurn = false;
-        }
-
         runTurn.Update();
 
-        TurnEnd();
-        SetTrap();
-        PlayMagic();
-        PlayMagicInit();
-        PutStone();
-        UseItem();
-        TurnStart();
-    }
+        if (!changeTurn) return;
 
-    void TurnStart()
-    {
-        if (mainStep != MainStep.StartTurn) return;
+        runTurn.Next();
+        runTurn = null;
+        runTurn = turnClass[(int)mainStep];
+        runTurn.Init();
+        changeTurn = false;
 
-        mainStep = MainStep.UseItem;
-    }
-
-    void UseItem()
-    {
-        if (mainStep != MainStep.UseItem) return;
-
-        gameManager.RegistScript(selectItem);
-
-        mainStep = MainStep.PutStone;
-    }
-
-    void PutStone()
-    {
-        if (mainStep != MainStep.PutStone) return;
-
-        gameManager.RegistScript(selectStone);
-
-        mainStep = MainStep.PlayMagicInit;
-    }
-
-    void PlayMagicInit()
-    {
-        if (mainStep != MainStep.PlayMagicInit) return;
-
-        tmpNowPlayerCount = gameManager.nowPlayerCount;
-        playMagicStep = PlayMagicStep.StartStep;
-        mainStep = MainStep.PlayMagic;
-    }
-
-    void PlayMagic()
-    {
-        if (mainStep != MainStep.PlayMagic) return;
-
-        EndStep();
-        UseCard();
-        SelectCard();
-        StartStep();
-
-    }
-
-    void SetTrap()
-    {
-        if (mainStep != MainStep.SetTrap) return;
-
-        gameManager.RegistScript(selectSetItem);
-
-
-        mainStep = MainStep.EndTurn;
-    }
-
-    void TurnEnd()
-    {
-        if (mainStep != MainStep.EndTurn) return;
-
-        mainStep = MainStep.StartTurn;
-        gameManager.AddNowPlayerCount();
-    }
-
-    void StartStep()
-    {
-        if (playMagicStep != PlayMagicStep.StartStep) return;
-
-        playMagicStep = PlayMagicStep.SelectCard;
-    }
-
-    void SelectCard()
-    {
-        if (playMagicStep != PlayMagicStep.SelectCard) return;
-
-        playMagicStep = PlayMagicStep.UseCard;
-        gameManager.RegistScript(selectMagic);
-
-    }
-
-    void UseCard()
-    {
-        if (playMagicStep != PlayMagicStep.UseCard) return;
-
-        playMagicStep = PlayMagicStep.EndStep;
-
-        if (gameManager.stackCount <= 0) return;
-
-        playCardUserCount = tmpNowPlayerCount + otherPlayerCount;
-        playCardUserCount %= gameManager.playersCount;
-
-        playMagicStep = PlayMagicStep.StartStep;
-        gameManager.RegistScript(selectCard);
-
-        otherPlayerCount++;
-        gameManager.SetNowPlayerCount(playCardUserCount + otherPlayerCount);
-
-    }
-
-    void EndStep()
-    {
-        if (playMagicStep != PlayMagicStep.EndStep) return;
-
-        testPlayCardCount++;
-        if (testPlayCardCount < gameManager.playersCount) return;
-        testPlayMagicCount++;
-        testPlayCardCount = 0;
-
-        gameManager.SetNowPlayerCount(tmpNowPlayerCount + testPlayMagicCount);
-
-        if (testPlayMagicCount < gameManager.playersCount) return;
-
-        testPlayMagicCount = 0;
-        gameManager.SetNowPlayerCount(tmpNowPlayerCount);
-        mainStep = MainStep.SetTrap;
     }
 }
