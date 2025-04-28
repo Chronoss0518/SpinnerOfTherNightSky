6556fed2ee2d273509a57972df83b03f2483a060
Chronoss0518 <sinyusuke5000@gmail.com>
2025-04-28 20:32:37 +0900

    ItemZoneを選択するためのScriptFunctionを追加

---
+++ Assets/Script/Game/Manager/ScriptFunctions/SelectItemZoneFunction.cs
@@ -0,0 +1,52 @@
+public class SelectItemZoneFunction : ScriptManager.ScriptFunctionBase
+{
+    public SelectItemZoneFunction(ScriptManager _manager) : base(_manager)
+    {
+        controller.Init(_manager);
+        SetSelectItemZoneFunctionController(controller);
+    }
+
+    public void Init()
+    {
+        if (isInit) return;
+        controller.ClearTarget();
+
+        isInit = true;
+    }
+
+    public override void Release()
+    {
+        isInit = false;
+    }
+
+    public override ScriptManager.ScriptArgument GenerateArgument(ScriptParts _script)
+    {
+        var res = new ScriptManager.SelectItemZoneArgument();
+
+        res.type = ScriptManager.ScriptType.SelectItemZone;
+
+        var args = GenerateArgument(_script.arguments);
+
+        for (int i = 0; i < args.Count; i++)
+        {
+            if (args[i].IndexOf("--") != 0) continue;
+
+            if (args[i] == "--target-player")
+                res.selectTarget = 1;
+
+            if (args[i] == "--target-other")
+                res.selectTarget = 2;
+        }
+
+        return res;
+    }
+
+    public override bool Run(ControllerBase _controller, GameManager _gameManager, ScriptManager.ScriptArgument _script)
+    {
+        Init();
+        return controller.SelectAction(_controller, _gameManager, _script);
+    }
+
+    bool isInit = false;
+    SelectItemZoneFunctionController controller = new SelectItemZoneFunctionController();
+}
---
+++ Assets/Script/Game/Manager/ScriptFunctions/SelectItemZoneFunctionController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using static ScriptManager;
+
+public class SelectItemZoneFunctionController : SelectScriptControllerBase
+{
+
+    [SerializeField, ReadOnly]
+    ScriptManager manager = null;
+
+    public int TargetPos { get; private set; } = -1;
+
+
+    public override void ClearTarget()
+    {
+        TargetPos = -1;
+    }
+
+    public void Init(ScriptManager _manager)
+    {
+        manager = _manager;
+    }
+
+    public void SelectPos(int _pos, GameManager _manager, SelectItemZoneArgument _runArgument)
+    {
+        if (_runArgument == null) return;
+        if (_pos < 0) return;
+        if (_pos >= ItemZoneManager.PUT_ITEM_COUNT) return;
+        if(TargetPos != -1 && TargetPos != _pos)
+        {
+            manager.SetError(ErrorType.IsNotTargetZone);
+            return;
+        }
+
+        TargetPos = TargetPos == _pos ? -1 : _pos;
+    }
+
+
+    public override bool SelectAction(ControllerBase _controller, GameManager _gameManager, ScriptArgument _script)
+    {
+        if (_script.type != ScriptType.SelectItemZone) return false;
+        StartManager(_gameManager);
+
+        var act = (SelectItemZoneArgument)_script;
+
+        string message = "";
+
+        if (manager.GetErrorType() == ErrorType.None)
+        {
+            message = $"配置先のItemZoneを指定してください。\n{(TargetPos >= 0 ? "選択済み" : "未選択")}";
+        }
+        else
+        {
+            if (manager.GetErrorType() == ErrorType.IsNotTargetZone) message = $"選択した場所は既にカードが置かれています。";
+            if (manager.GetErrorType() == ErrorType.IsRangeMinOverCount) message = $"配置先のItemZoneが選択されていません。";
+
+            manager.DownErrorMessageDrawCount();
+        }
+
+        _gameManager.SetMessate(message);
+
+        if (!_controller.isAction) return true;
+        if (TargetPos < 0)
+        {
+            manager.SetError(ErrorType.IsRangeMinOverCount);
+            _controller.DownActionFlg();
+            return true;
+        }
+
+        _controller.ActionEnd();
+        manager.AddUseScriptCount();
+        _gameManager.EndSelectCardTest();
+        startFlg = false;
+        return true;
+    }
+
+    private void StartManager(GameManager _manager)
+    {
+        if (startFlg) return;
+
+        startFlg = true;
+    }
+
+    bool startFlg = false;
+
+}
