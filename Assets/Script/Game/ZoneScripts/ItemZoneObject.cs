25b4bf84f864041e418823bf408ff1b3d9f87866
Chronoss0518 <sinyusuke5000@gmail.com>
2025-04-29 15:42:02 +0900

    ItemZoneの1カードを管理するクラスを追加

---
+++ Assets/Script/Game/ZoneScripts/ItemZoneObject.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using Unity.Collections;
+
+public class ItemZoneObject : MonoBehaviour
+{
+
+    public CardScript itemCard { get { return card; } }
+
+    public void SetItemCard(CardData _card, Player _player, GameManager _gameManager,bool openFlg = false)
+    {
+        if (_gameManager == null) return;
+        if (_gameManager.cardPrefab == null) return;
+
+        var cardObj = Instantiate(_gameManager.cardPrefab.gameObject,transform);
+
+        cardObj.transform.localPosition = Vector3.zero;
+        cardObj.transform.localRotation = Quaternion.identity;
+
+        card = cardObj.GetComponent<CardScript>();
+
+        card.Init(_player, _gameManager, _card, ScriptManager.ZoneType.ItemZone);
+    }
+
+    public void RemoveCard()
+    {
+        if (card == null) return;
+        Destroy(card);
+        card = null;
+    }
+
+    public bool IsCardData(CardData _card) { return card.IsCardData(_card); }
+
+    public bool IsPutCard() { return card != null; }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    [SerializeField, ReadOnly]
+    bool openFlg = false;
+    bool beforeOpenFlg = false;
+
+    [SerializeField,ReadOnly]
+    CardScript card = null;
+}
