8c760db217b78d1e7764ff58fdd91ff9bdab9535
Chronoss0518 <sinyusuke5000@gmail.com>
2025-04-30 13:44:47 +0900

    OpenItemZoneのメソッドをCardDataからも展開できるように修正

--- Assets/Script/Game/Manager/ScriptFunctions/OpenItemZoneCardFunction.cs
+++ Assets/Script/Game/Manager/ScriptFunctions/OpenItemZoneCardFunction.cs
@@ -13,13 +13,38 @@ public class OpenItemZoneCardFunction : ScriptManager.ScriptFunctionBase
 
     public override bool Run(ControllerBase _controller, GameManager _gameManager, ScriptManager.ScriptArgument _script)
     {
+        ItemZonePosTest();
+        TargetCardTest(_controller);
 
-        if (targetItemZonePos == null) return true;
-        if (targetItemZonePos.IsOpen()) return true;
+        AddUseScriptCount();
+        return true;
+    }
+
+    void TargetCardTest(ControllerBase _controller)
+    {
+        var player = _controller.GetComponent<Player>();
+        if (player == null) return;
+        if (targetCards.Count > player.itemZone.nowCanPutCount) return;
+
+        foreach(var card in targetCards)
+        {
+            if (card.zone.zoneType != ScriptManager.ZoneType.ItemZone) continue;
+            var itemZone = (ItemZoneManager)card.zone;
+            if (itemZone == null) continue;
+            itemZone.OpenCard(card.baseData);
+        }
+
+        return;
+    }
+
+
+    void ItemZonePosTest()
+    {
+        if (targetItemZonePos == null) return;
+        if (targetItemZonePos.IsOpen()) return;
 
         targetItemZonePos.SetOpenFlg(true);
 
-        return true;
     }
 
 }
