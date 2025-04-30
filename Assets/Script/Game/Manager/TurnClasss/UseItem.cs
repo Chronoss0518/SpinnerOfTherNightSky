3602c9bb95d6f5012cbe89e88b1028f55fe9ae27
Chronoss0518 <sinyusuke5000@gmail.com>
2025-04-30 13:45:26 +0900

    UseItemのスクリプトにOpenItemZoneCardを追加

--- Assets/Script/Game/Manager/TurnClasss/UseItem.cs
+++ Assets/Script/Game/Manager/TurnClasss/UseItem.cs
@@ -23,16 +23,17 @@ public class UseItem : TurnManager.TurnClass
     {
         selectItem = gameManager.CreateScript(new ScriptData(
             new ScriptParts[] {
-                new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--min 0 --max 1 --player-type 0 --zone-type-book --card-type 2 --normal-playing"),
+                new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--min 0 --max 1 --player-type 0 --zone-type-book --zone-type-item --card-type 2 --normal-playing"),
                 new ScriptParts((int)ScriptManager.ScriptType.SelectItemZone, "--target-player"),
                 new ScriptParts((int)ScriptManager.ScriptType.MoveCard, "--open-item-zone"),
+                new ScriptParts((int)ScriptManager.ScriptType.OpenItemZoneCard,""),
                 new ScriptParts((int)ScriptManager.ScriptType.Stack, ""),},
             ScriptManager.ArgumentType.Entry));
 
         selectTrap = gameManager.CreateScript(new ScriptData(
             new ScriptParts[] {
                 new ScriptParts((int)ScriptManager.ScriptType.SelectCard, "--min 0 --max 1 --player-type 0 --zone-type-item --card-type 4 --normal-playing"),
-                new ScriptParts((int)ScriptManager.ScriptType.MoveCard, "--open-item-zone"),
+                new ScriptParts((int)ScriptManager.ScriptType.OpenItemZoneCard, "--open-item-zone"),
                 new ScriptParts((int)ScriptManager.ScriptType.Stack, ""),},
             ScriptManager.ArgumentType.Entry));
     }
