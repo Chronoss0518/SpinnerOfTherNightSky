d05a6ea0d4fa85158f3f4d02973814dd743ab77d
Chronoss0518 <sinyusuke5000@gmail.com>
2025-05-04 13:53:41 +0900

    CPUに一旦Wait機能を追加

--- Assets/Script/Game/Controller/CPUController.cs
+++ Assets/Script/Game/Controller/CPUController.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class CPUController : ControllerBase
 {
+    [SerializeField]
+    int maxWaitCount = 100;
+
+    [SerializeField,ReadOnly]
+    int nowWaitCount = 0;
 
     // Update is called once per frame
     void Update()
     {
+        if (!isControll) return;
+
+        nowWaitCount++;
+        if (maxWaitCount > nowWaitCount) return;
+
+        nowWaitCount = 0;
         UpActionFlg();
     }
 }
