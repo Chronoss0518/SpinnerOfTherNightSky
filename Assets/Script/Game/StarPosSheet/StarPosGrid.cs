701511a6325619cfaf7cdb4d2879b44eef859d76
Chronoss0518 <sinyusuke5000@gmail.com>
2025-05-17 15:41:28 +0900

    StarPosGridスクリプトを作成

---
+++ Assets/Script/Game/StarPosSheet/StarPosGrid.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Unity.Collections;
+
+public class StarPosGrid : MonoBehaviour, IPointerDownHandler
+{
+    public Vector3 touchPosition { get; private set; } = Vector3.zero;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        touchPosition = eventData.pointerPressRaycast.worldPosition;
+    }
+
+
+}
